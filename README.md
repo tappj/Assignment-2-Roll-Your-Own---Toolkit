d3e7d433d8b190f9be3d3bc1beaaec7af5002b8f
Jadon Tapp <tappj@uci.edu>
2026-04-27 21:49:57 -0700

    updated changes

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # svg-widget-toolkit
 Starter repository for creating widgets using SVG.js
-# Assignment-2-Roll-Your-Own---Toolkit
-
--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,23 @@ import {Window} from "./core/ui"
 import {Button} from "./widgets/button"
 import {Heading} from "./widgets/heading"
 
+let w = new Window(window.innerHeight - 10, '100%');
 
-let w = new Window(window.innerHeight-10,'100%');
-
-let lbl1= new Heading(w);
+let lbl1 = new Heading(w);
 lbl1.text = "Button Demo";
 lbl1.tabindex = 1;
-lbl1.fontSize = 16;
-lbl1.move(10,20);
+lbl1.fontSize = 24;
+lbl1.move(40, 40);
 
 let btn = new Button(w);
 btn.tabindex = 2;
-btn.fontSize = 14
-btn.move(12, 50)
+btn.label = "Click Me";
+btn.fontSize = 16;
+btn.size = { width: 120, height: 44 };
+btn.move(40, 100);
+
+let clickCount = 0;
+btn.onClick(() => {
+    clickCount++;
+    lbl1.text = `Button clicked ${clickCount} time${clickCount === 1 ? '' : 's'}!`;
+});
